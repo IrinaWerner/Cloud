6d2f23139c9b49847893cdc5775c255c8e6d9cf8
User Kucherenko <raito-werner@yandex.ru>
2024-02-14 12:20:13 +0300

    Планы поменялись, удалили статус домейн сёрвис и всё, что с ним связано

--- src/main/java/ru/mycloud/cloud/controller/UserController.java
+++ src/main/java/ru/mycloud/cloud/controller/UserController.java
@@ -1,14 +1,8 @@
 package ru.mycloud.cloud.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import ru.mycloud.cloud.dto.request.UserStatusRequest;
-import ru.mycloud.cloud.entity.user.UserStatus;
 import ru.mycloud.cloud.service.UserService;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,16 +10,9 @@ public class UserController {
 
     private final UserService service;
 
-    private static final  String USER_STATUS_GET = "/api/v1/user-status/get";
 
 
-    @PostMapping(
-            value = USER_STATUS_GET,
-            consumes = APPLICATION_JSON_VALUE,
-            produces = APPLICATION_JSON_VALUE)
 
-    public UserStatus getUserStatus(@RequestBody UserStatusRequest request){
-        return service.getUserStatus(request.getId());
-    }
+
 
 }
